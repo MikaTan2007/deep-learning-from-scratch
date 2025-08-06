767b95e552cc0b209fccaa2265f5f437aeae1ac4
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-08-06 17:53:28 +0800

    Derivative function

--- chapter_1/main.py
+++ chapter_1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import ndarray
+from typing import Callable
 
 def square (x: ndarray) -> ndarray:
     #returns squared function
@@ -7,3 +8,10 @@ def square (x: ndarray) -> ndarray:
 
 def leaky_relu(x: ndarray) -> ndarray:
     return np.maximum(0.2 * x, x)
+
+def deriv(func: Callable[[ndarray], ndarray],
+          input_: ndarray,
+          delta: float = 0.001) -> ndarray:
+    
+    return (func(input_ + delta) - func(input_-delta)/(2 * delta))
+
