92649a7a8bc3e873c4aab4138ef779155e8139ed
Spinduck5384 <67084559+MikaTan2007@users.noreply.github.com>
2025-08-10 00:03:45 +0800

    Update main.py

--- chapter_1/main.py
+++ chapter_1/main.py
@@ -13,5 +13,5 @@ def deriv(func: Callable[[ndarray], ndarray],
           input_: ndarray,
           delta: float = 0.001) -> ndarray:
     
-    return (func(input_ + delta) - func(input_-delta)/(2 * delta))
+    return (func(input_ + delta) - (func(input_-delta)/(2 * delta)))
 
